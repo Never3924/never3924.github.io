84addf02784898100e07f3067f9eecd324f67792
Never3924 <120770059+Never3924@users.noreply.github.com>
2022-12-27 17:39:02 +0900

    Update header.js

--- js/header.js
+++ js/header.js
@@ -1,4 +1,25 @@
 document.addEventListener('DOMContentLoaded',()=>{
+  //head
   document.head.innerHTML+='<title>Never3924 | '+document.getElementById('pageinside').className+'</title>';
   document.head.innerHTML+='<link href="https://fonts.googleapis.com/css?family=M+PLUS+1p" rel="stylesheet"></link>';
+  
+  //body
+  var newhead=document.createElement('header');
+  newhead.id='header';
+  newhead.style.backgroundColor='#349934';
+  
+  var newa=document.createElement('a');
+  newa.href='/';
+  newa.id='hddiv';
+  newa.style='text-align:center;weight:100%;';
+  
+  var newhdd=document.createElement('div');
+  newhdd.id='text';
+  newhdd.textContent='Never3924 Blog (?)'
+  
+  //element add
+  newa.appendChild(newhdd);
+  newhead.appendChild(newa);
+  
+  document.body.prepend(newhead);
 });
