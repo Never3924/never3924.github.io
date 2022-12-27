0ecba3308a118c9838d3b5ab07c5dbcd208b9417
Never3924 <120770059+Never3924@users.noreply.github.com>
2022-12-27 17:22:10 +0900

    Update header.js

--- js/header.js
+++ js/header.js
@@ -1,4 +1,4 @@
 document.addEventListener('DOMContentLoaded',()=>{
-  document.head.innerHTML+='<title>Never3924 | '+document.getElementById('pageinside').value+'</title>';
+  document.head.innerHTML+='<title>Never3924 | '+document.getElementById('pageinside').className+'</title>';
   document.head.innerHTML+='<link href="https://fonts.googleapis.com/css?family=M+PLUS+1p" rel="stylesheet"></link>';
 });
