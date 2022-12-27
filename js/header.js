0187f336a7f9e5cc83ab4809ae811b605c359ccc
Never3924 <120770059+Never3924@users.noreply.github.com>
2022-12-27 17:51:36 +0900

    test

--- js/header.js
+++ js/header.js
@@ -6,12 +6,12 @@ document.addEventListener('DOMContentLoaded',()=>{
   //body
   var newhead=document.createElement('header');
   newhead.id='header';
-  newhead.style='background-color:#0062ff;color:#ffffff;font-weight:10px;position:absolute;top:0px;left:0px;height:100px;';
+  newhead.style='background-color:#0062ff;font-weight:10px;position:absolute;top:0px;left:0px;height:100px;width:100%;';
   
   var newa=document.createElement('a');
   newa.href='/';
   newa.id='hddiv';
-  newa.style='text-align:center;text-decoration:none;';
+  newa.style='text-align:center;text-decoration:none;color:#ffffff;';
   
   var newhdd=document.createElement('div');
   newhdd.id='text';
