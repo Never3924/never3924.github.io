ce5182383910a7b2be0c76e56b4df8413086242a
Never3924 <120770059+Never3924@users.noreply.github.com>
2026-01-06 11:46:32 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import path from "path";
 
 const nextConfig: NextConfig = {
     /* config options here */
+
+    async rewrites() {
+        return [
+            {
+                source: "/games/4x4LifeGame/",
+                destination: "/html/4x4LifeGame/index.html",
+            },
+        ];
+    },
 };
 
 export default nextConfig;
